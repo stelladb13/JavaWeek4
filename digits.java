b955acb75a2e532c375b633ead64d16b3fb0b01f
stelladb13 <sdbertoli3@gmail.com>
2025-02-26 09:52:44 -0500

    Update digits.java

--- digits.java
+++ digits.java
@@ -9,16 +9,20 @@ public class digits{
         int ones;
         int tens;
         int hundreds;
+        int thousands;
         int num;
 
-        num = 258;
+        num = 2258;
         hundreds = ((num/10)%10)/2;
         tens = (num/10)%10;
         ones = num%10;
+        thousands = (((num/10)%10)/2)%10;
+
         
-        System.out.println(ones);
-        System.out.println(tens);
-        System.out.println(hundreds);
+        System.out.println("The ones digit is " + ones);
+        System.out.println("The tens digit is " + tens);
+        System.out.println("The hundreds digit is " + hundreds);
+        System.out.println("The thousands digit is " + thousands);
         
     }
 
